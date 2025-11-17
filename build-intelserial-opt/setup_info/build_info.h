ccf61e4b60999f3de6e29e7e9354cf8cfde5e6fb
juanjoaucar <93452131+juanjoaucar@users.noreply.github.com>
2025-11-16 22:40:01 -0300

    Add first reports

---
+++ build-intelserial-opt/setup_info/build_info.h
@@ -0,0 +1,44 @@
+#ifndef BUILD_INFO_H_INCLUDED
+#define BUILD_INFO_H_INCLUDED
+
+#define USER_NAME                "root"
+#define HOST_NAME                "runner-vu-uzivwu-project-67998188-concurrent-0"
+#define SYSTEM                   "Linux-6.2.0-32-generic"
+#define CMAKE_VERSION            "4.1.2"
+#define CMAKE_GENERATOR          "Unix Makefiles"
+#define CMAKE_BUILD_TYPE         "release"
+#define CONFIGURATION_TIME       "2025-10-28 00:05:44.610970+00:00"
+
+#define FORTRAN_COMPILER_ID      "IntelLLVM"
+#define FORTRAN_COMPILER         "/opt/intel/oneapi/compiler/2025.3/bin/ifx"
+#define FORTRAN_COMPILER_VERSION "2025.3.0"
+#define FORTRAN_COMPILER_FLAGS   " -O3 -xHost -w -assume byterecl -g -traceback -DVAR_IFX -fiopenmp"
+
+#define C_COMPILER_ID            "IntelLLVM"
+#define C_COMPILER               "/opt/intel/oneapi/compiler/2025.3/bin/icx"
+#define C_COMPILER_VERSION       "2025.3.0"
+#define C_COMPILER_FLAGS         " -O3 -xHost -g -fiopenmp"
+
+#define CXX_COMPILER_ID          "IntelLLVM"
+#define CXX_COMPILER             "/opt/intel/oneapi/compiler/2025.3/bin/icpx"
+#define CXX_COMPILER_VERSION     "2025.3.0"
+#define CXX_COMPILER_FLAGS       " -O3 -xHost -Wno-unknown-pragmas -fiopenmp"
+
+#define STATIC_LINKING           "False"
+#define ENABLE_64BIT_INTEGERS    "False"
+#define ENABLE_MPI               "False"
+#define MPI_LAUNCHER             "unknown"
+
+#define MATH_LIBS                "unknown"
+#define ENABLE_BUILTIN_BLAS      "OFF"
+#define ENABLE_BUILTIN_LAPACK    "OFF"
+#define EXPLICIT_LIBS            "unknown"
+
+#define ENABLE_EXATENSOR         "ON"
+#define EXATENSOR_REPO           "https://github.com/RelMBdev/ExaTENSOR.git"
+#define EXATENSOR_CONFIG         "WRAP=NOWRAP BUILD_TYPE=OPT EXA_TALSH_ONLY=YES CMAKE_Fortran_COMPILER=/opt/intel/oneapi/compiler/2025.3/bin/ifx CMAKE_C_COMPILER=/opt/intel/oneapi/compiler/2025.3/bin/icx CMAKE_CXX_COMPILER=/opt/intel/oneapi/compiler/2025.3/bin/icpx TOOLKIT=INTEL EXA_OS=LINUX GPU_CUDA=NOCUDA MPILIB=NONE BLASLIB=NONE     "
+#define EXATENSOR_HASH           "a1efa9efdcae13caacdc4776af839c3cd1957e85"
+
+#define DEFINITIONS              "HAVE_MKL_BLAS;HAVE_MKL_LAPACK;HAVE_OPENMP;SYS_LINUX;PRG_DIRAC;INSTALL_WRKMEM=64000000;EXA_TALSH_ONLY;BUILD_GEN1INT;HAS_PELIB;MOD_LAO_REARRANGED;MOD_MCSCF_spinfree;MOD_XAMFI;MOD_ESR;MOD_KRCC;MOD_SRDFT;MOD_EXACORR"
+
+#endif /* BUILD_INFO_H_INCLUDED */
---
+++ html/build-intelmpi-noopt/setup_info/build_info.h
@@ -0,0 +1,44 @@
+#ifndef BUILD_INFO_H_INCLUDED
+#define BUILD_INFO_H_INCLUDED
+
+#define USER_NAME                "root"
+#define HOST_NAME                "runner-vu-uzivwu-project-67998188-concurrent-0"
+#define SYSTEM                   "Linux-6.2.0-32-generic"
+#define CMAKE_VERSION            "4.1.2"
+#define CMAKE_GENERATOR          "Unix Makefiles"
+#define CMAKE_BUILD_TYPE         "release"
+#define CONFIGURATION_TIME       "2025-10-27 21:53:07.100178+00:00"
+
+#define FORTRAN_COMPILER_ID      "IntelLLVM"
+#define FORTRAN_COMPILER         "/opt/intel/oneapi/mpi/2021.17/bin/mpiifx"
+#define FORTRAN_COMPILER_VERSION "2025.3.0"
+#define FORTRAN_COMPILER_FLAGS   " -w -assume byterecl -g -traceback -DVAR_IFX  -fiopenmp"
+
+#define C_COMPILER_ID            "IntelLLVM"
+#define C_COMPILER               "/opt/intel/oneapi/mpi/2021.17/bin/mpiicx"
+#define C_COMPILER_VERSION       "2025.3.0"
+#define C_COMPILER_FLAGS         " -g  -fiopenmp"
+
+#define CXX_COMPILER_ID          "IntelLLVM"
+#define CXX_COMPILER             "/opt/intel/oneapi/mpi/2021.17/bin/mpiicpx"
+#define CXX_COMPILER_VERSION     "2025.3.0"
+#define CXX_COMPILER_FLAGS       " -Wno-unknown-pragmas  -fiopenmp"
+
+#define STATIC_LINKING           "False"
+#define ENABLE_64BIT_INTEGERS    "False"
+#define ENABLE_MPI               "True"
+#define MPI_LAUNCHER             "/opt/intel/oneapi/mpi/2021.17/bin/mpiexec"
+
+#define MATH_LIBS                "-Wl,--start-group;/opt/intel/oneapi/mkl/2025.3/lib/intel64/libmkl_lapack95_lp64.a;/opt/intel/oneapi/mkl/2025.3/lib/intel64/libmkl_intel_lp64.so;-qopenmp;-Wl,--end-group;-Wl,--start-group;/opt/intel/oneapi/mkl/2025.3/lib/intel64/libmkl_intel_lp64.so;/opt/intel/oneapi/mkl/2025.3/lib/intel64/libmkl_intel_thread.so;/opt/intel/oneapi/mkl/2025.3/lib/intel64/libmkl_core.so;/usr/lib/x86_64-linux-gnu/libpthread.a;/usr/lib/x86_64-linux-gnu/libm.so;-qopenmp;-Wl,--end-group"
+#define ENABLE_BUILTIN_BLAS      "OFF"
+#define ENABLE_BUILTIN_LAPACK    "OFF"
+#define EXPLICIT_LIBS            "unknown"
+
+#define ENABLE_EXATENSOR         "OFF"
+#define EXATENSOR_REPO           "unknown"
+#define EXATENSOR_CONFIG         "unknown"
+#define EXATENSOR_HASH           "unknown"
+
+#define DEFINITIONS              "HAVE_MKL_BLAS;HAVE_MKL_LAPACK;HAVE_MPI;HAVE_OPENMP;VAR_MPI;VAR_MPI2;USE_MPI_MOD_F90;SYS_LINUX;PRG_DIRAC;INSTALL_WRKMEM=64000000;BUILD_GEN1INT;HAS_PELIB;MOD_LAO_REARRANGED;MOD_MCSCF_spinfree;MOD_XAMFI;MOD_ESR;MOD_KRCC;MOD_SRDFT"
+
+#endif /* BUILD_INFO_H_INCLUDED */
